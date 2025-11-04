c60074136c2155a894d5527f75a83a96981057db
Patel <savan2@illinois.edu>
2025-11-04 12:38:39 -0600

    Ready for deployment

--- models/user.js
+++ models/user.js
@@ -20,16 +20,48 @@ UserSchema.pre('save', async function (next) {
             return next();
         }
 
-        const Task = require("./task");
-        const taskIds = this.pendingTasks.map(id => new mongoose.Types.ObjectId(id));
-        const tasks = await Task.find({ _id: { $in: taskIds } });
+        const Task = require('./task');
+        const existingUser = await this.constructor.findById(this._id);
 
-        for (const task of tasks) {
-            if (task.assignedUser !== this._id.toString()) {
-                task.assignedUser = this._id.toString();
+        const oldTaskIds = existingUser ? existingUser.pendingTasks.map(String) : [];
+        const newTaskIds = this.pendingTasks.map(String);
+
+        const removedTaskIds = oldTaskIds.filter(id => !newTaskIds.includes(id));
+        const addedTaskIds = newTaskIds.filter(id => !oldTaskIds.includes(id));
+        
+        // Name change for user
+        if (existingUser && existingUser.name !== this.name) {
+            const assignedTasks = await Task.find({ assignedUser: this._id.toString() });
+            for (const task of assignedTasks) {
                 task.assignedUserName = this.name;
                 task.__updateFromUser = true;
                 await task.save();
+            }
+        }
+
+        // Remove tasks that aren't in the new tasks
+        if (removedTaskIds.length > 0) {
+            const oldTasks = await Task.find({ _id: { $in: removedTaskIds } });
+            for (const task of oldTasks) {
+                if (task.assignedUser === this._id.toString()) {
+                    task.assignedUser = '';
+                    task.assignedUserName = 'unassigned';
+                    task.__updateFromUser = true;
+                    await task.save();
+                }
+            }
+        }
+
+        // Add tasks that aren't in the old tasks
+        if (addedTaskIds.length > 0) {
+            const newTasks = await Task.find({ _id: { $in: addedTaskIds } });
+            for (const task of newTasks) {
+                if (task.assignedUser !== this._id.toString()) {
+                    task.assignedUser = this._id.toString();
+                    task.assignedUserName = this.name;
+                    task.__updateFromUser = true;
+                    await task.save();
+                }
             }
         }
 
